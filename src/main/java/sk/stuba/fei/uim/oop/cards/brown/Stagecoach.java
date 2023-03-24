547b5d0f57a71dfe1c278fe2d0317231ebaef6e6
Marek Sýkorka <marcopolo1234565@gmail.com>
2023-03-24 11:58:35 +0100

    Final Game - v 1.0.1  - Stagecoach Bug FIX

--- src/main/java/sk/stuba/fei/uim/oop/cards/brown/Stagecoach.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/brown/Stagecoach.java
@@ -1,5 +1,6 @@
 package sk.stuba.fei.uim.oop.cards.brown;
 
+import sk.stuba.fei.uim.oop.cards.Card;
 import sk.stuba.fei.uim.oop.deck.Deck;
 import sk.stuba.fei.uim.oop.player.Player;
 import sk.stuba.fei.uim.oop.utility.TxtDef;
@@ -15,9 +16,10 @@ public class Stagecoach extends BrownCard {
 
     @Override
     public void play(Player activePlayer, ArrayList<Player> enemyPlayers, Deck deck) {
+        ArrayList<Card> drawnCards = deck.drawCards(2);
+        deck.setStatusMessage(TxtDef.CLI_INFO + activePlayer.getName() + " -> " + drawnCards.size() + " cards drawn.");
         deck.discardCard(activePlayer.removeCardsOnHand(this));
-        deck.setStatusMessage(TxtDef.CLI_INFO + activePlayer.getName() + " -> Two cards drawn.");
-        activePlayer.setCardsOnHand(deck.drawCards(2));
+        activePlayer.setCardsOnHand(drawnCards);
     }
 
     @Override
